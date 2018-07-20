0f1813da7b0da2547ec4b89f511987c9cf5f8453
epeee <epeee@users.noreply.github.com>
2018-07-20 20:27:35 +0200

    Add sample code

---
+++ api/src/main/java/io/github/epeee/shipkit/demo/api/Service.java
@@ -0,0 +1,6 @@
+package io.github.epeee.shipkit.demo.api;
+
+public interface Service {
+
+    boolean doSomething();
+}
---
+++ impl/src/test/java/io/github/epeee/shipkit/demo/impl/SampleServiceTest.java
@@ -0,0 +1,13 @@
+package io.github.epeee.shipkit.demo.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SampleServiceTest {
+
+    @Test
+    void doSomething() {
+        assertTrue(new SampleService().doSomething());
+    }
+}
