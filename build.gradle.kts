1b6be670578fdbe83be76d8c179e355d3f4db247
epeee <epeee@users.noreply.github.com>
2019-09-09 20:03:21 +0200

    Update junit (v5.5.2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ subprojects {
         plugin("java-library")
     }
 
-    val junitJupiterVersion = "5.5.1"
+    val junitJupiterVersion = "5.5.2"
 
     val testRuntimeOnly by configurations
     val testImplementation by configurations
