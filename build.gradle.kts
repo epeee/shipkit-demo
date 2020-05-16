2a171279f07372b56ab1ce5cb605121c1cc0d077
epeee <epeee@users.noreply.github.com>
2020-05-16 12:02:19 +0200

    Update gradle (v6.5-milestone-2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,6 @@ subprojects {
 }
 
 tasks.named<Wrapper>("wrapper") {
-    gradleVersion = "6.5-milestone-1"
-    distributionSha256Sum = "7e6ac72a36af92474ee8c4ddae73cbc3cc5e6be33fb088ff6668465b25addcd9"
+    gradleVersion = "6.5-milestone-2"
+    distributionSha256Sum = "3e269c619d1d19cbbcbd74165487a973d8ad307def9552883e601a93b60ced4b"
 }
